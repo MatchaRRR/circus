5bd6b4c21e81b956e0b100b70de3af8854ca5471
fuyixuan <yixuanfu762@gmail.com>
2024-09-13 17:17:16 +0800

    use ArrayList

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -1,12 +1,13 @@
 package circus;
 
-import circus.animal.Animal;
-import circus.animal.Duck;
-import circus.animal.Parrot;
-import circus.animal.Tiger;
+import circus.animal.*;
 import circus.stuff.Cannon;
 import circus.stuff.Equipment;
 import circus.stuff.Ladder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Circus {
     private static Animal[] animals = {
@@ -42,9 +43,36 @@ public class Circus {
         return total;
     }
 
+    private static void printAllAnimals(ArrayList<Animal> animalList) {
+        for (Animal a : animalList) {
+            System.out.println(a);
+        }
+    }
+
     public static void main(String[] args) {
-        makeAnimalsTalk();
-        System.out.println("Total value of animals " + calculateAssetValue(animals));
-        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+        System.out.println("The total number of animals is " + animals.length);
+        // animals[3] = new Elephant("Eli"); // fixed size array, does not work.
+        ArrayList<Animal> animalList = new ArrayList<>(Arrays.asList(animals));
+
+        // size and add
+        printAllAnimals(animalList);
+        System.out.println("Size of animal arrayList is " + animalList.size());
+        Elephant elephant = new Elephant("Eli");
+        animalList.add(elephant);
+        Duck duck = new Duck("Donald");
+        animalList.add(duck);
+        System.out.println();
+
+        // sorting
+        printAllAnimals(animalList);
+        System.out.println("Size of animal arrayList is " + animalList.size());
+        System.out.println("The index of the elephant in the ArrayList is " + animalList.indexOf(elephant));
+        animalList.sort(Animal.comparator); // need a comparator, defined in Animal.java.
+        printAllAnimals(animalList); // The ArrayList has been sorted alphabetically.
+
+//        makeAnimalsTalk();
+//        System.out.println("Total value of animals " + calculateAssetValue(animals));
+//        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
     }
 }
+// other data structure: HashMap, Key-value paris
--- src/main/java/circus/animal/Animal.java
+++ src/main/java/circus/animal/Animal.java
@@ -2,9 +2,18 @@ package circus.animal;
 
 import circus.Asset;
 
+import java.util.Comparator;
+
 // Prevent creating Animal Class instance.
 public abstract class Animal implements Asset {
-
+    public static Comparator<Animal> comparator = new Comparator<Animal>() {
+        @Override
+        public int compare(Animal animal1, Animal animal2) {
+            // This method returns an integer whose sign is that of calling compareTo with case folded versions of the strings
+            // where case differences have been eliminated by calling Character. toLowerCase(Character. toUpperCase(int)) on each Unicode code point.
+            return animal1.name.compareToIgnoreCase(animal2.name);
+        }
+    };
     public String name;
     public abstract String speak();
 }
